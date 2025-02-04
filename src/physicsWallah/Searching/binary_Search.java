11c05ec827c283c6fcd575f2645f1ec429480635
AnujKourav <anujkourav63@gmail.com>
2025-02-05 05:20:09 +0530

    Adding Binary Search

---
+++ src/physicsWallah/Searching/binary_Search.java
@@ -0,0 +1,40 @@
+package physicsWallah.Searching;
+
+//implementing binary search
+
+public class binary_Search {
+
+    //boolean method, if found return true else return false
+    static boolean search(int []arr,int target){
+
+        //creating pointers
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<=end){
+
+            //finding mid
+            int mid = (start + end )/ 2;
+
+            if(target == arr[mid]){
+                return true;
+            }
+            else if(target > arr[mid]){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        //passing sorted array
+        int []arr = {1,2,3,4,5};
+
+        System.out.println(search(arr,6));
+
+    }
+}
