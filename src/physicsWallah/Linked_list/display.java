d0b3b244c808d246a3c2611cc42349d34ef06515
AnujKourav <anujkourav63@gmail.com>
2025-02-11 21:42:51 +0530

    Adding Linked List

--- src/physicsWallah/Linked_list/display.java
+++ src/physicsWallah/Linked_list/display.java
@@ -7,6 +7,19 @@ public class display {
         Node(int data){
             this.data = data;
         }
+        public static void printList(Node head){ //display using function
+            Node temp = head; //for preserving the head
+            while(temp != null){
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+        public static void displayr(Node head){ //display using recursion
+            if(head == null)return; //base case
+            System.out.print(head.data+" ");
+            displayr(head.next);
+        }
     }
     public static void main(String[] args) {
         Node a = new Node(5);
@@ -20,17 +33,27 @@ public class display {
         c.next = d;
         d.next = e;
 
+        //display using for loop
         Node temp = a;
         for(int i=1;i<=5;i++){ // number of element is known
             System.out.print(temp.data+" ");
             temp = temp.next;
         }
 
+        //display using while loop
         System.out.println();
         temp = a;
         while(temp != null){ //size is not given
             System.out.print(temp.data +" ");
             temp = temp.next;
         }
+
+        //display using function
+        System.out.println();
+        Node.printList(a);
+
+        //display using recursion
+        System.out.println();
+        Node.displayr(a);
     }
 }
