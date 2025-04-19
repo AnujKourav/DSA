7fe6ebf897b996a57706273dc3e883e6eae30fc6
AnujKourav <anujkourav63@gmail.com>
2025-04-20 01:53:55 +0530

    Adding Map Interface

---
+++ src/physicsWallah/MapInterface/Treemap.java
@@ -0,0 +1,48 @@
+package physicsWallah.MapInterface;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Treemap {
+    public static void main(String[] args) {
+        Map<Integer, String> mp = new TreeMap<>();
+        mp.put(3,"Aman");
+        mp.put(1,"Rohan");
+        mp.put(2,"Riya");
+        System.out.println(mp); // -> sorted according to keys
+        System.out.println(mp.get(2)); // Riya
+        System.out.println(mp.containsKey(4));
+        System.out.println(mp.containsValue("Aman"));
+        mp.put(1,"anuj");
+        System.out.println(mp); // -> over-rides
+        mp.putIfAbsent(2,"Ram"); // -> do not over-ride
+        System.out.println(mp);
+        System.out.println(mp.entrySet()); // -> printing whole mapping
+        System.out.println(mp.keySet()); // -> print only keys
+        System.out.println(mp.values()); // -> print only values
+
+        //Iterating over keys in a map
+        for(Integer i: mp.keySet()){
+            System.out.println(i);
+        }
+        //Iterating over values in a map
+        for(String i: mp.values()){
+            System.out.println(i);
+        }
+        //Iterating over values in a map
+        for(var i: mp.values()){ // -> also we can use var if we don't want to define data type
+            System.out.println(i);
+        }
+        //Iterating over key-values in a map
+        for(var i: mp.entrySet()){
+            System.out.println(i);
+        }
+        //Iterating over key in a map
+        for(var i: mp.entrySet()){
+            System.out.println(i.getKey());
+        }
+        //Iterating over values in a map
+        for(var i: mp.entrySet()){
+            System.out.println(i.getValue());
+        }
+    }
+}
